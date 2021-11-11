66327f413253f2fbe72f3706f3729a90825b7a39
ImKrishnaMadala <88415843+ImKrishnaMadala@users.noreply.github.com>
2021-11-10 23:32:57 -0500

    update landing page animation

--- swaperator.js
+++ swaperator.js
@@ -1,80 +1,124 @@
-// DOM selectors
-const stars = document.getElementById('stars');
-const starsCtx = stars.getContext('2d');
-const slider = document.querySelector(".slider input");
-const output = document.querySelector("#speed");
+var tickInterval;
 
-// global variables
-let screen, starsElements, starsParams = { speed: 2, number: 300, extinction: 4 };
+//_______________________
 
-// run stars
-setupStars();
-updateStars();
-
-// handle slider
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
-    starsParams.speed = this.value;
-};
-
-// update stars on resize to keep them centered
-window.onresize = function() {
-    setupStars();
-};
-
-// star constructor
-function Star() {
-    this.x = Math.random() * stars.width;
-    this.y = Math.random() * stars.height;
-    this.z = Math.random() * stars.width;
-
-    this.move = function() {
-        this.z -= starsParams.speed;
-        if (this.z <= 0) {
-            this.z = stars.width;
+// Hack for missing JSON file
+Date.prototype.CLOCK_DESCRIPTIONS = [
+ {
+    'hours': 
+        {
+             '1': 'one',
+             '2': 'two',
+             '3': 'three',
+             '4': 'four',
+             '5': 'five(?!x)',
+             '6': 'six',
+             '7': 'seven',
+             '8': 'eight',
+             '9': 'nine',
+            '10': 'ten(?!f)',
+            '11': 'eleven',
+            '12': 'twelve'
+        },
+    
+    'minutes': 
+        {
+             '0': 'oclock',
+             '5': 'five(?!t)|past',
+            '10': 'ten(?!s)|past',
+            '15': 'quarter|past',
+            '20': 'twenty|past',
+            '25': 'twentyfive|past',
+            '30': 'half|past',
+            '35': 'twentyfive|to',
+            '40': 'twenty|to',
+            '45': 'quarter|to',
+            '50': 'ten(?!s)|to',
+            '55': 'five(?!t)|to'
         }
-    };
+       
+ }
+];
 
-    this.show = function() {
-        let x, y, rad, opacity;
-        x = (this.x - screen.c[0]) * (stars.width / this.z);
-        x = x + screen.c[0];
-        y = (this.y - screen.c[1]) * (stars.width / this.z);
-        y = y + screen.c[1];
-        rad = stars.width / this.z;
-        opacity = (rad > starsParams.extinction) ? 1.5 * (2 - rad / starsParams.extinction) : 1;
+Date.prototype.getTimeAsText = function() {
+	var hours, minutes, guessMins, i = this.getMinutes();
+	var period = this.getHours();
+		period = period >= 12 ? 'pm' : 'am';
+		
+	// Get minutes text description for every increment of 5, and return
+	// description, if not multiple of 5 check value is 58 or 59 if true
+	// return oclock, else round to nearest multiple of 5
+	minutes =  this.CLOCK_DESCRIPTIONS[0].minutes[this.getMinutes()];
+  
+	if ( typeof minutes == 'undefined' ) {
+	  
+	  if ( i > 57 && i <= 59 ) {
+		  minutes = this.CLOCK_DESCRIPTIONS[0].minutes[0]; // return oclock
+	  } else {
+	  	guessMins = Math.round( this.getMinutes() / 5 ) * 5;; // Round to nearest 5
+		  minutes =  this.CLOCK_DESCRIPTIONS[0].minutes[guessMins];
+	  }
+	  
+	}
+  
+	// Convert getHours 0-23 to 12hr clock
+	hours = this.getHours();
+		if ( hours > 12 ) {
+			hours -= 12;
+		} else if ( hours === 0 ) {
+			hours = 12;
+		}
+  
+	// Increment hour for minutes greater than 32, push the hour back to 1 if at 12
+	if ( i > 32 && hours < 12 ) {	
+		hours++; 
+	} else if ( i > 32 ) {
+		hours = 1;
+	}
+   
+	// Match hour to text description from config
+	hours = this.CLOCK_DESCRIPTIONS[0].hours[hours];
+  
+		return '(it(?=l)|is(?=t)|' + minutes + '|' + hours + '|' + period + ')';
+};
 
-        starsCtx.beginPath();
-        starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
-        starsCtx.arc(x, y, rad, 0, Math.PI * 2);
-        starsCtx.fill();
-    }
+function setTimeString( e ) {
+	// Sets the pattern of text
+	var patternString = 'itlistempusquarterkrdvtwentyfivexhalfbtenftopasterunineonesixthreefourfivetwoeightelevenseventwelvetenseoclockamxpmxfugit';
+  
+		e.innerHTML = patternString;
 }
 
-// setup <canvas>, create all the starts
-function setupStars() {
-    screen = {
-        w: window.innerWidth,
-        h: window.innerHeight,
-        c: [ window.innerWidth * 0.5, window.innerHeight * 0.5 ]
-    };
-    window.cancelAnimationFrame(updateStars);
-    stars.width = screen.w;
-    stars.height = screen.h;
-    starsElements = [];
-    for (let i = 0; i < starsParams.number; i++) {
-        starsElements[i] = new Star();
-    }
+function illuminateTime( e ) {
+	// Get element by ID
+	e = document.getElementById( e );
+
+	// Get date as text string
+	var d = new Date();
+	// New regex with formatted date text string '(match|alternation)' 
+	var re = new RegExp( d.getTimeAsText(), 'gi' );
+
+	// Reset time string 
+    	setTimeString( e );
+	// Replace content of e with regex matches wrapped in span
+		e.innerHTML = e.innerHTML.replace( re, "<span class='illuminate'>$1</span>" );
+	
+		tick();
 }
 
-// redraw the frame
-function updateStars() {
-    starsCtx.fillStyle = "black";
-    starsCtx.fillRect(0, 0, stars.width, stars.height);
-    starsElements.forEach(function (s) {
-        s.show();
-        s.move();
-    });
-    window.requestAnimationFrame(updateStars);
+function tick() {
+	var tick, d = new Date(); // Interval declared in global scope
+	
+	clearInterval( tickInterval);
+
+	tick = ( 60 - d.getSeconds() ) * 1000;
+		( tick == 0 ) ? tick = 60000 : tick;
+		
+	    // Set interval to re-call every tick, will fire at 1 second past the minute
+	    tickInterval = setInterval( function() {
+	    	illuminateTime( 'tempus-fugit' ); 
+	    }, tick );
+		
 }
+
+illuminateTime( 'tempus-fugit' );
